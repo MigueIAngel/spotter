b97a42e502cee6813cb90d0a96160a4ae4f83b97
Miguel Angel <miguelaltamar@uninorte.edu.co>
2023-11-20 23:15:32 -0500

    progreso

---
+++ lib/config/configuration.dart
@@ -0,0 +1,11 @@
+class Configuration {
+  static const apiKey = "AIzaSyA9Dm3bo8lWBE704wjGo05q-wTBPFhQ77I";
+  static const authDomain = "spotter-7cb61.firebaseapp.com";
+  static const databaseURL =
+      "https://spotter-7cb61-default-rtdb.firebaseio.com";
+  static const projectId = "spotter-7cb61";
+  static const storageBucket = "spotter-7cb61.appspot.com";
+  static const messagingSenderId = "425810974783";
+  static const appId = "1=425810974783=web=29f5c460801137c6c4408a";
+  static const measurementId = "G-7N7HS6Y6R7";
+}
